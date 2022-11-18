bb4c4e0913a7947c685c3b33dbdea5ea881f4cf5
Yuri Guller <gullerya@gmail.com>
2022-11-18 14:57:17 +0200

    remove output

--- data/transformer.js
+++ data/transformer.js
@@ -10,80 +10,85 @@ const rl = readline.createInterface({
 
 let words = [];
 let currentObjectLines = null;
-const lexes = {};
-const suffixes = {};
+const suffixes = {
+	"00 ": "׃",
+	"&": "־",
+	"05 ": "׀",
+	"00_P ": "׃פ",
+	"00_S ": "׃ס",
+	"_P ": "פ",
+	"00_N_S ": "׃נס",
+	"00_N_P ": "׃נפ",
+	"_S ": "ס",
+	" 05 ": "׀",
+	"00_N ": "׃׆"
+};
 
+const startTime = performance.now();
+
+let lineNumber = 0;
 for await (const line of rl) {
 	if (line === 'CREATE OBJECT') {
 		currentObjectLines = [];
 	} else if (line === ']' && Array.isArray(currentObjectLines)) {
-		const currentWord = processObject(currentObjectLines);
-		words.push(currentWord);
+		const currentWord = processObject(currentObjectLines, lineNumber);
+		if (currentWord.word) {
+			words.push(currentWord);
+		}
 		currentObjectLines = null;
 	} else if (currentObjectLines) {
 		currentObjectLines.push(line);
 	}
 
-	if (words.length > 100) {
-		break;
-	}
+	lineNumber++;
+
+	// if (words.length > 100) {
+	// 	break;
+	// }
 }
 
 fs.rmSync('data/output', { recursive: true });
 fs.mkdirSync('data/output', { recursive: true });
 fs.writeFileSync('data/output/words.json', JSON.stringify(words, null, 4), { encoding: 'utf-8' });
-fs.writeFileSync('data/output/lexes.json', JSON.stringify(lexes, null, 4), { encoding: 'utf-8' });
 fs.writeFileSync('data/output/suffixes.json', JSON.stringify(suffixes, null, 4), { encoding: 'utf-8' });
+
+console.log(`done in ${Math.floor(performance.now() - startTime)}ms`);
 
 //	PROCESSORS
 
-function processObject(inputLines) {
+function processObject(inputLines, startingLineNumber) {
 	const result = {};
 
-	let lexemeCount;
 	let suffixPlain;
-	for (const line of inputLines) {
+	inputLines.forEach((line, lineNumber) => {
 		if (line.includes('ID_D')) {
-			result.id = parseInt(line.split('=').pop());
+			// result.id = parseInt(line.split('=').pop());
 		} else if (line.startsWith('g_word_utf8')) {
 			result.word = asciiToUtf(line.split(':=').pop());
 		} else if (line.startsWith('g_voc_lex_utf8')) {
 			result.lex = asciiToUtf(line.split(':=').pop());
-			collectLexemeCount(result.lex, lexemeCount);
-		} else if (line.startsWith('lexeme_count')) {
-			lexemeCount = parseInt(line.split(':=').pop());
-			collectLexemeCount(result.lex, lexemeCount);
 		} else if (line.startsWith('distributional_parent')) {
-			result.distParent = parseInt(line.split(':=').pop());
+			// result.distParent = parseInt(line.split(':=').pop());
 		} else if (line.startsWith('functional_parent')) {
-			result.funcParent = parseInt(line.split(':=').pop());
+			// result.funcParent = parseInt(line.split(':=').pop());
 		}
 
-		if (line.startsWith('g_suffix')) {
+		if (line.startsWith('g_suffix_utf8')) {
+			const utfSuffix = asciiToUtf(line.split(':=').pop());
+			if (utfSuffix && utfSuffix !== ' ') {
+				if (!suffixPlain) throw new Error(`suffixes are mixed up, line: ${lineNumber + startingLineNumber}, suffix plain: [${suffixPlain}], word: ${JSON.stringify(result)}`);
+				suffixes[suffixPlain] = utfSuffix;
+				suffixPlain = null;
+			}
+		} else if (line.startsWith('g_suffix')) {
 			suffixPlain = line.split(':=').pop().replace(/[\";]/g, '');
-		} else if (line.startsWith('g_suffix_utf8')) {
-			if (!suffixPlain) throw new Error('suffixes are mixed up');
-			suffixes[suffixPlain] = asciiToUtf(line.split(':=').pop());
 		}
-	}
+	});
 
 	//	validations
 	//if (!result.id) throw new Error(`ID is missing on ${JSON.stringify(result)}`);
 
 	return result;
-}
-
-function collectLexemeCount(lexeme, count) {
-	if (!lexeme || !count) {
-		return;
-	}
-	if (lexeme in lexes) {
-		if (lexes[lexeme] !== count) {
-			throw new Error(`for lexeme ${lexeme} found counts contradiction: ${lexes[lexeme]} vs ${count}`);
-		}
-	} else {
-		lexes[lexeme] = count;
-	}
 }
 
 function asciiToUtf(asciiInput) {
@@ -108,8 +113,8 @@ function asciiToUtf(asciiInput) {
 // g_word_utf8:="\xd7\x94\xd6\xb7";	//	handled
 // g_cons_utf8:="\xd7\x94";
 // g_cons:="H";
-// g_suffix:="";
-// g_suffix_utf8:="";
+// g_suffix:="";					//	handled
+// g_suffix_utf8:="";				//	handled
 // g_pfm:="";
 // g_pfm_utf8:="";
 // g_vbs:="";
