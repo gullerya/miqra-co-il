d817d798a34ab58838245fbb6dfa003aeddee5d6
Yuri Guller <gullerya@gmail.com>
2022-11-27 22:37:26 +0200

    clean up;

--- src/components/tmp/elements/selectable.js
+++
@@ -1,9 +0,0 @@
-customElements.define('select-able', class extends HTMLElement {
-	set selected(selected) {
-		if (selected) {
-			this.classList.add('selected');
-		} else {
-			this.classList.remove('selected');
-		}
-	}
-});
